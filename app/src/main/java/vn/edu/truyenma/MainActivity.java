f5d4eda69af7d66447dc7164b1480e8993d9b302
HoangTien <hoangtienvhdh@gmail.com>
2023-12-21 17:48:11 +0700

    lên lại nào

--- app/src/main/java/vn/edu/truyenma/MainActivity.java
+++ app/src/main/java/vn/edu/truyenma/MainActivity.java
@@ -1,15 +1,63 @@
 package vn.edu.truyenma;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.webkit.WebSettings;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    TextView txt;
 
     // khai bao các biến toàn cục nơi đây;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Show_menu();
+            }
+        });
     }
+    public void Show_menu(){
+        PopupMenu show_menu = new PopupMenu(this , btn);
+        show_menu.getMenuInflater().inflate(R.menu.show_menu ,show_menu.getMenu());
+        show_menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                txt = findViewById(R.id.textView);
+                int itemId = item.getItemId();
+               if(R.id.item1 == itemId){
+                   WebView webView = findViewById(R.id.webView);
+
+                   // Thiết lập các cài đặt của WebView
+                   WebSettings webSettings = webView.getSettings();
+                   webSettings.setJavaScriptEnabled(true); // Cho phép chạy mã JavaScript
+
+                   // Load file index.html từ thư mục assets
+                   webView.loadUrl("file:///android_asset/index.html");
+               }else if (R.id.item2 == itemId) {
+                   txt.setText("Số 2");
+               }else if(R.id.item3== itemId){
+                   txt.setText("Số 3");
+               }else if(R.id.item4== itemId){
+                   txt.setText("Số 4");
+               }else{
+                   txt.setText("Lỗi rồi");
+               }
+                return false;
+            }
+        });
+
+        show_menu.show();
+    }
+
 }
