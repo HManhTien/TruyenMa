91b06b44af68b169b10804be01db141bd2234343
HoangTien <hoangtienvhdh@gmail.com>
2023-12-21 10:16:17 +0700

    len lan dau tien

---
+++ app/src/main/java/vn/edu/truyenma/MainActivity.java
@@ -0,0 +1,15 @@
+package vn.edu.truyenma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // khai bao các biến toàn cục nơi đây;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
